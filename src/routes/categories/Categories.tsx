de6f3311243d5eacb96b5b1eddb5314ab41451aa
einargudnig <einargudnig@gmail.com>
2019-04-19 14:40:14 +0000

    HATA CLASSNAMES

--- src/routes/categories/Categories.tsx
+++ src/routes/categories/Categories.tsx
@@ -3,7 +3,6 @@ import './Categories.scss';
 import { getCategories } from '../../api/index';
 import { IProduct, ICategory} from '../../api/types';
 import { Link } from 'react-router-dom';
-import classNames from 'classnames';
 
 export default function CategoriesRoute(props: any) {
   const { category, onClick, notClickable } = props;
@@ -31,7 +30,7 @@ export default function CategoriesRoute(props: any) {
   return (
     <div className="categories">
       {categories.map((category) => (
-      <Link to={`/categories/${category.id}`} onClick={(e: any) => click(e, category.id)} className={classNames("category",notClickable && "notClickable")}>
+      <Link to={`/categories/${category.id}`} onClick={(e: any) => click(e, category.id)} className="category">
           <div
             key={category.id}
             className="categories__box"
--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -71,12 +71,3 @@ export default function Home() {
     </Fragment>
   );
 }
-
-
-/**
- * {data.map((i:any ) => {
- *  <Product
- *    productDetails={i}
- *  ></Product>
- * })}
- */
