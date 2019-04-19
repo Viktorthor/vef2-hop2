5de0807873f12f1bf1330994ea3f399e84a71885
einargudnig <einargudnig@gmail.com>
2019-04-19 14:14:43 +0000

    rosa gaman

--- src/routes/categories/Categories.tsx
+++ src/routes/categories/Categories.tsx
@@ -3,13 +3,13 @@ import './Categories.scss';
 import { getCategories } from '../../api/index';
 import { IProduct, ICategory} from '../../api/types';
 import { Link } from 'react-router-dom';
-//import classNames from 'classnames';
+import classNames from 'classnames';
 
 export default function CategoriesRoute(props: any) {
   const { category, onClick, notClickable } = props;
   console.log('P', props);
 
-  //const [products, setProducts] = useState([] as IProduct[]);
+  const [products, setProducts] = useState([] as IProduct[]);
   const [categories, setCategories] = useState([] as ICategory[]);
   const [loading, setLoading] = useState(false);
 
@@ -32,14 +32,14 @@ export default function CategoriesRoute(props: any) {
     <div className="categories">
       {categories.map((category) => (
 
-//       <Link to={`/categories/${category.id}`} onClick={(e: any) => click(e, category.id)} className={classNames("category",notClickable && "notClickable")}>
+       <Link to={`/categories/${category.id}`} onClick={(e: any) => click(e, category.id)} className={classNames("category",notClickable && "notClickable")}>
           <div
             key={category.id}
             className="categories__box"
           >
             <p className="categories__title">{category.title}</p>
           </div>
-//        </Link>
+        </Link>
     ))}
   </div>
   )
